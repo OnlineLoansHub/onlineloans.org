6e6ad18e7502cf6e632dd36e1807b73b5760a1bf
wedjat11 <jamontiel@ammper.com>
2025-01-30 10:33:00 -0600

    fix:scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,31 +13,35 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang='en'>
+      {/* Google Tag Manager */}
       <Script
-        async={true}
+        async
         src='https://www.googletagmanager.com/gtag/js?id=AW-16834519489'
-      ></Script>
-      <Script>
+      />
+      <Script id='google-analytics'>
         {`
-             window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'AW-16834519489');
-`}
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16834519489');
+        `}
       </Script>
-      <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "q22rqfm6yn");
-</script>
+
+      {/* Microsoft Clarity */}
+      <Script id='clarity'>
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "q22rqfm6yn");
+        `}
+      </Script>
 
       <body className='antialiased bg-white'>{children}</body>
     </html>
