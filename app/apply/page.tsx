029694855edb74c3d057d67dc0cf436019f6e4a0
wedjat11 <jesusalfonsomontiel@gmail.com>
2025-01-31 12:41:28 -0600

    clarity added to apply

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -3,8 +3,10 @@ import { MontserratFont } from '@/app/Fonts'
 
 import MainFooter from './components/Footer'
 import Main from './components/Main'
+import { useClarity } from '../Clarity'
 
 const Test: React.FC = () => {
+  useClarity()
   return (
     <main className={MontserratFont.className}>
       <Main />
